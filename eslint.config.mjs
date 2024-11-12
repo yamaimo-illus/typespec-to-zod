59496d56c57fe93c05abcb226e8dfbb3269db4e7
yamaimo-illus <yamaimo.illustration@gmail.com>
2024-11-12 16:26:02 +0900

    fix: eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,3 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  rules: {
-    'no-use-before-define': 'off',
-    'ts/no-use-before-define': 'off',
-  },
-})
+export default antfu()
